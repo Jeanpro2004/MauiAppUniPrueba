558f9f935228aafaebfdda1c923829d6d946cf99
Jeanpro2004 <jean.rodriguez.ortiz@udla.edu.ec>
2025-07-13 18:25:30 -0500

    Agregar archivos de proyecto.

---
+++ MauiAppUniPrueba/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppUniPrueba
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
